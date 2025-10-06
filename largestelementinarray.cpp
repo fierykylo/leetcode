8f579c0a12effcc088009d42119f9f56cd0fcb8c
Aarush Dubey <mrknownunknown2702@gmail.com>
2025-10-06 19:25:41 +0530

    question

---
+++ largestelementinarray.cpp
@@ -0,0 +1,19 @@
+//https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0
+#include <bits/stdc++.h>
+
+class Solution {
+  public:
+    int largest(vector<int> &arr)
+    {
+        // code here
+        int largest = arr[0];
+        for(int i : arr)
+        {
+            if(i > largest)
+            {
+                largest = i;
+            }
+        }
+        return largest;
+    }
+};
