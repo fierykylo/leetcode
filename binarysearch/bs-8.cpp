d8e28648e45fd39f88a5627f3d682a0a19771153
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-01-20 16:48:20 +0530

    https://leetcode.com/problems/single-element-in-a-sorted-array/submissions/1890929079/

---
+++ binarysearch/bs-8.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+//https://leetcode.com/problems/single-element-in-a-sorted-array/submissions/1890929079/
+
+class Solution
+{
+    public:
+        int singleNonDuplicate(vector<int> &nums)
+        {
+            int n = nums.size();
+
+            if (n == 1)
+            {
+                return nums[0];
+            }
+            if (nums[0] != nums[1])
+            {
+                return nums[0];
+            }
+            if (nums[n - 1] != nums[n - 2])
+            {
+                return nums[n - 1];
+            }
+
+            int high = n - 2;
+            int low = 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+
+                if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1])
+                {
+                    return nums[mid];
+                }
+                if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || 
+                    (mid % 2 == 0 && nums[mid] == nums[mid + 1]))
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            } 
+            return -1;
+        }
+        
+};
