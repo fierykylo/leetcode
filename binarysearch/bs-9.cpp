9ced37d606b28a87b500ac25c56990495c6cb4b0
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-01-21 19:07:40 +0530

    https://leetcode.com/problems/find-peak-element/submissions/1892172623/

--- binarysearch/bs-9.cpp
+++ binarysearch/bs-9.cpp
@@ -1,6 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 // https://leetcode.com/problems/find-peak-element/
 
+class Solution
+{
+public:
+    int findPeakElement(vector<int> &nums)
+    {
+        // handling edge cases
+
+        int n = nums.size();
+
+        if (n == 1)
+        {
+            return 0;
+        }
+        if (nums[0] > nums[1])
+        {
+            return 0;
+        }
+        if (nums[n - 1] > nums[n - 2])
+        {
+            return (n - 1);
+        }
+
+        int low = 1;
+        int high = n - 2;
+
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+            {
+                return mid;
+            }
+            else if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
