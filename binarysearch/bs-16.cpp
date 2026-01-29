066720fdd243adf2a5f7f44428ea5090fbf4fa3a
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-01-29 11:51:23 +0530

    Kth missing positive number

---
+++ binarysearch/bs-16.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://leetcode.com/problems/kth-missing-positive-number/submissions/1900593268/
+// this one is a great question and we used some maths to derive it
+
+class Solution 
+{
+public:
+    int findKthPositive(vector<int>& arr, int k) 
+    {
+        int low = 0;
+        int high = arr.size() - 1;
+        
+        while(low <= high)
+        {
+            int mid = (low + high) / 2;
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return (low + k);
+    }
+};
