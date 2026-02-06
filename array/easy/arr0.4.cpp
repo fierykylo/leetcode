e5dafb03f3a95edf80c0e29e19b19474574f9da5
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-02-07 02:57:41 +0530

    set mismatch

---
+++ array/easy/arr0.4.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// array - II question 1
+//https://leetcode.com/problems/set-mismatch/?envType=problem-list-v2&envId=dsa-linear-shoal-array-ii
+class Solution 
+{
+public:
+    vector<int> findErrorNums(vector<int>& nums) 
+    {
+
+        int n = nums.size();
+        int dup = -1;
+        int missing = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if(nums[abs(nums[i]) - 1] < 0)
+            {
+                dup = abs(nums[i]);
+            }
+            else
+            {
+                nums[abs(nums[i]) - 1] *= (-1);
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] > 0)
+            {
+                missing = i + 1;
+                break;
+            }
+        }
+        return {dup, missing};
+
+    }
+};
