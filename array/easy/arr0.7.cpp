c3877caa3e4e6336f6d14488da72cdb5f0794545
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-02-09 02:54:41 +0530

    Build an array with stack operations

---
+++ array/easy/arr0.7.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// two pointer approach
+// stimulation greedy
+// leetcode medium but ts is easy and its more of a quest problem so i just put it here
+// anyways marked as a stack since we are stimulating a stack
+//https://leetcode.com/problems/build-an-array-with-stack-operations/?envType=problem-list-v2&envId=dsa-linear-shoal-stack
+
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) 
+    {
+        vector<string> operations;
+        int j = 0;
+        int size = target.size();
+        for (int i = 0; i < n, j < size; i++)
+        {
+            if ((i + 1) == target[j])
+            {
+                operations.push_back("Push");
+                j++;
+            }
+            else if ((i + 1) != target[j])
+            {
+                operations.push_back("Push");
+                operations.push_back("Pop");
+            }
+        }
+        return operations;
+
+
+    }
+};
