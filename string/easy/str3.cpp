8ee2a02dafddacee6fee2935d912d5c7dabe8caf
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-02-12 09:18:47 +0530

    largest odd number in a string

---
+++ string/easy/str3.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//https://leetcode.com/problems/largest-odd-number-in-string/submissions/1916474822/
+
+class Solution 
+{
+public:
+    string largestOddNumber(string num) 
+    {
+        int n = num.size() - 1;
+        for (int i = n; i >= 0; i--)
+        {
+            if ((num[i] - '0') % 2 == 1)
+            {
+                return num.substr(0, i + 1);
+            }
+        }
+        return "";
+    }
+};
