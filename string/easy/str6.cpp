4717c8debf23a1594fcc548a814e4093d7e0b92d
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-02-18 01:03:51 +0530

    Rotate string

--- string/easy/str6.cpp
+++ string/easy/str6.cpp
@@ -1,3 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+//https://leetcode.com/problems/rotate-string/
+
+class Solution 
+{
+public:
+    bool rotateString(string s, string goal) 
+    {
+        int n = s.length();
+        int m = goal.length();
+
+        if (n != m) return false;
+        
+        if ((s + s).find(goal) != string::npos)
+        {
+            return true;
+        }
+        return false;
+        
+    }
+};
