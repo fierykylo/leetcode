aeb9f0dee5eb6edc512d39cfaba6f0f41a95141c
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-03-10 09:25:47 +0530

    az101

---
+++ az/az101/day3.cpp
@@ -0,0 +1,15 @@
+// Write your code here
+#include <bits/stdc++.h>
+using namespace std;
+//basic data types
+int main(void)
+{
+    int var1;
+    long long var2;
+    char var3;
+    float var4;
+    double var5;
+
+    cin >> var1 >> var2 >> var3 >> var4 >> var5;
+    cout << var1 << endl << var2 << endl << var3 << endl << var4 << endl << var5 << endl;   
+}
