7ef5f6863cb24ead0db7d1566c97580316252443
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-03-11 10:23:20 +0530

    simple calculator

---
+++ az/az101/day5.0.cpp
@@ -0,0 +1,14 @@
+// Write your code here
+#include <bits/stdc++.h>
+using namespace std;
+
+//simple calculator
+
+int main(void)
+{
+    long long int x , y;
+    cin >> x >> y;
+    cout << x << " + " << y << " = " << x + y << endl;
+    cout << x << " * " << y << " = " << x * y << endl;
+    cout << x << " - " << y << " = " << x - y << endl;
+}
