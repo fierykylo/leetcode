b4806e598dcb4d76921c55c4d390c41f24ed3b65
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-03-12 20:07:32 +0530

    linked list cycle

--- llidk5.cpp
+++ llidk5.cpp
@@ -10,7 +10,7 @@
      ListNode() : val(0), next(nullptr) {}
      ListNode(int x) : val(x), next(nullptr) {}
      ListNode(int x, ListNode *next) : val(x), next(next) {}
- }
+ };
 class Solution {
 public:
     bool hasCycle(ListNode *head) 
