7d2686e259f7e20b33fbc6912506c079ad0269d0
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-03-26 15:52:04 +0530

    sum multiples

---
+++ array/easy/arr4.0.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//https://leetcode.com/problems/sum-multiples/description/
+
+class Solution {
+public:
+    int sumOfMultiples(int n) 
+    {
+        int sum = 0;
+        for (int i = 3; i <= n; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0 | i % 7 == 0)
+            {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+};
