f031bffa1e4f006d4ec40ccde502f8982ba2d793
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-04-07 15:58:03 +0530

    implement queue using stacks leetcode 232

---
+++ az/phase2/week5/ass7.1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//https://leetcode.com/problems/implement-queue-using-stacks/submissions/1971454105/
+
+class MyQueue {
+public:
+    stack<int> in, out;
+
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        in.push(x);
+    }
+    
+    int pop() {
+        if(out.empty()) {
+            transfer();
+        }
+        int val = out.top();
+        out.pop();
+        return val;
+    }
+    
+    int peek() {
+        if(out.empty()) {
+            transfer();
+        }
+        return out.top();
+    }
+    
+    bool empty() {
+        return in.empty() && out.empty();
+    }
+
+private:
+    void transfer() {
+        while(!in.empty()) {
+            out.push(in.top());
+            in.pop();
+        }
+    }
+};
