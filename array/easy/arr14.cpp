d1ebffad26e7ddb457f9209877be5bcf2123e004
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-04-22 00:16:41 +0530

    difference of Sums

--- array/easy/arr14.cpp
+++ array/easy/arr14.cpp
@@ -1,2 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+//https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/submissions/1984755696/?envType=problem-list-v2&envId=prshgx6i
+
+class Solution {
+public:
+    int differenceOfSums(int n, int m) 
+    {
+        int num1 = 0;
+        int num2 = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if(i % m == 0)
+            {
+                num2 += i;
+            }
+            else
+            {
+                num1 += i;
+            }
+        }
+        return num1-num2;
+    }
+};
