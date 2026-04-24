80e5c5d44545473f8a3a9ae562a90dd32d321c1f
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-04-24 17:06:06 +0530

    fruthest point from origin

---
+++ daily/daily32.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//https://leetcode.com/problems/furthest-point-from-origin/?envType=daily-question&envId=2026-04-24
+//24-04-26
+
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) 
+    {
+        int dist1 = 0;
+        int dist2 = 0;
+
+        for (auto ch : moves)
+        {
+            if (ch == 'L')
+            {
+               dist1 -= 1;
+               dist2 -= 1; 
+            }
+            else if (ch == 'R')
+            {
+                dist1 += 1;
+                dist2 += 1;
+            }
+            else
+            {
+                dist1 -= 1; //moves left
+                dist2 += 1; // moves right
+            }
+        }
+        dist1 = abs(dist1);
+        dist2 = abs(dist2);
+        return max(dist1, dist2);
+    }
+};
