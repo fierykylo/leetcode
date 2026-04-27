fc0da4f3410be9fcc27623aea1674c54fb10835e
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-04-27 22:35:14 +0530

    find the maximum achievable number

---
+++ array/easy/arr17.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//https://leetcode.com/problems/find-the-maximum-achievable-number/description/?envType=problem-list-v2&envId=prshgx6i
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) 
+    {
+        return (num + 2 * t);
+    }
+};
