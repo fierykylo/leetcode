b87184637454805389b29a0edc5405223bb1a26a
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-04-29 16:53:18 +0530

    find words containing character

---
+++ array/easy/arr18.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/find-words-containing-character/submissions/1990973073/?envType=problem-list-v2&envId=prshgx6i
+
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) 
+    {
+        vector<int> ans;
+        for(int i = 0; i < words.size(); i++)
+        {
+            if(words[i].find(x) != string::npos)
+            {
+                ans.push_back(i);
+            }
+
+        } 
+        return ans;  
+    }
+};
