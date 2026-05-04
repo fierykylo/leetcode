28e2d747e139b82998b5bb1c11191bcca1ed98a6
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-05-04 06:23:31 +0530

    rotate image

---
+++ daily/daily36.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//https://leetcode.com/problems/rotate-image/?envType=daily-question&envId=2026-05-04
+
+//4th may 2026
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) 
+    {
+        // two steps to solve this problem 
+        // transpose then reverse 
+
+        // transpose
+        int n = matrix.size();
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        // reverse
+        for (int i = 0; i < n; i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
