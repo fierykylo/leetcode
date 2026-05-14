ab1440996d8be84851ab5d7a4d7317f273f2833a
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-05-14 22:25:15 +0530

    minimum moves to make array complementary

--- daily/may26/daily13.cpp
+++ daily/may26/daily13.cpp
@@ -13,7 +13,7 @@ public:
 
         vector<int> diff(2 * limit + 2, 0);
 
-        for(int i = 0; i < n/2; i++) {
+        for(int i = 0; i < n/2; i++) {  
 
             int a = nums[i];
             int b = nums[n-1-i];
