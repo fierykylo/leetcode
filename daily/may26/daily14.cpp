45e98f4765abcc008c532a56b2f6f6de8bfbb095
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-05-16 11:46:00 +0530

    check if array is good

---
+++ daily/may26/daily14.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//https://leetcode.com/problems/check-if-array-is-good/
+
+//14th may 2026
+
+
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int n = nums.size();
+
+        int maxEl = n-1;
+
+        int maxElCount = 0;
+
+        for(int num : nums) {
+            int val = abs(num);
+
+            if(val > maxEl) {
+                return false;
+            } else if(val == maxEl) {
+                maxElCount++;
+            }
+
+            if(nums[val] < 0) {
+                if(val != maxEl) {
+                    return false;
+                } else if(maxElCount > 2) {
+                    return false;
+                }
+            } else {
+                nums[val] *= -1;
+            }
+        }
+
+        return true;
+    }
+};
