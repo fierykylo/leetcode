d2264b90290cf9dd0228017bfcb93ccea908f9ff
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-08-04 21:32:03 +0530

    basic calculator

---
+++ az/101/fundamentals-1/fun3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//basic calculator 
+
+int main(void)
+{
+    long long int x , y;
+    cin >> x >> y;
+    cout << x << " + " << y << " = " << x + y << endl;
+    cout << x << " * " << y << " = " << x * y << endl;
+    cout << x << " - " << y << " = " << x - y << endl;
+}
