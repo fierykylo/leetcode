54ab82109e0dc8e2d1ba578e2669ea363169cdb5
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-08-04 22:09:04 +0530

    Difference

--- az/101/fundamentals-1/fun4.cpp
+++ az/101/fundamentals-1/fun4.cpp
@@ -1,2 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long 
+
+//https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+
+signed main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        int ans = (a * b) - (c * d);
+        cout << "Difference = " << ans << "\n";   
+    }
+}
