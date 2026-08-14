3714bda14eeda6cb78243598237cdd79f8557ed3
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-08-14 12:05:29 +0530

    structural changes

---
+++ az/101/fundamentals-1/fun13.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if(a >= b)
+        {
+            cout << "Yes\n";
+        }
+        else
+        {
+            cout << "No\n";
+        }
+    }
+}
---
+++ az/misc/stl/rand.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using  namespace std;
+
+int main(void)
+{
+    int arr[5];
+    cout << arr + 5 << endl;
+}
