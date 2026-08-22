5279bf7692025b6b1ed67914342312613f3ee46a
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-08-22 19:24:50 +0530

    String score

---
+++ az/AZ-TASKS/Week-02/Task-14/stringStore.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solution() {
+    int n; cin >> n;
+    string s; cin >> s;
+    int score = 0;
+    for (int i = 0; i < n; i++) {
+        char c = s[i];
+        switch (c) {
+        case 'V':
+            score += 5;
+            break;
+        case 'W':
+            score += 2;
+            break;
+        case 'Z':
+            if (i != n - 1) {
+                if (s[i + 1] == 'W') {
+                    score /= 2;
+                    s[i + 1] = '0';
+                } else if (s[i + 1] == 'V') {
+                    score /= 5;
+                    s[i + 1] = '0';
+                }
+            }
+            break;
+        case 'Y':
+            if (i != n - 1) {
+                s.push_back(s[i + 1]);
+                n++;
+                s[i + 1] = '0';
+            }
+            break;
+        case 'X':
+            if (i != n - 1) {
+                s[i + 1] = '0';
+            }
+        }
+    }
+    cout << score;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    solution();
+    return 0;
+}
